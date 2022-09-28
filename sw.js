18f46764011578a1b9c84cbf321a31559a90a167
UlisesIslas <20193tn013@utez.edu.mx>
2022-09-27 22:15:14 -0500

    root change

--- sw.js
+++ sw.js
@@ -3,13 +3,13 @@ self.addEventListener('install', (event) => {
     const cachePromise = caches.open('cache-v1').then((cache) => {
         return cache.addAll(
             [
-                '/',
-                '/index.html',
-                '/pages/sum.html',
-                '/pages/rest.html',
-                '/pages/mult.html',
-                '/pages/div.html',
-                '/js/app.js',
+                './',
+                './index.html',
+                './pages/sum.html',
+                './pages/rest.html',
+                './pages/mult.html',
+                './pages/div.html',
+                './js/app.js',
                 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css',
                 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js',
             ]
